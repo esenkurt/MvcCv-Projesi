306ae5c2e2a86ab4d2162ac3ba5b2983a6e2bc1a
Esen Kurtulmus <109716591+esen-kurtulmus@users.noreply.github.com>
2022-10-01 13:35:45 +0300

    Proje dosyası ekle.

---
+++ MvcCv/Controllers/DefaultController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+
+namespace MvcCv.Controllers
+{
+    [AllowAnonymous]
+    public class DefaultController : Controller
+    {
+        DbcvEntities db = new DbcvEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkimda.ToList();
+            return View(degerler);
+        }
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.TblDeneyimlerim.ToList();
+            return PartialView(deneyimler);
+        }
+        public PartialViewResult SosyalMedya()
+        {
+            var sosyalmedya = db.TblSosyalMedya.Where(x=> x.Durum==true).ToList();
+            return PartialView(sosyalmedya);
+        }
+
+        public PartialViewResult Egitimlerim()
+        {
+            var egitimler = db.TblEgitimlerim.ToList();
+            return PartialView(egitimler);
+        }
+        public PartialViewResult Yeteneklerim()
+        {
+            var yetenekler = db.TblYeteneklerim.ToList();
+            return PartialView(yetenekler);
+        }
+        public PartialViewResult Hobilerim()
+        {
+            var hobiler = db.TblHobilerim.ToList();
+            return PartialView(hobiler);
+        }
+        public PartialViewResult sertifikalar()
+        {
+            var sertifikalar = db.TblSertifikalarim.ToList();
+            return PartialView(sertifikalar);
+        }
+        [HttpGet]
+        public PartialViewResult iletisim()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult iletisim(Tbliletisim t)
+        {
+            t.Tarih =DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.Tbliletisim.Add(t);
+            db.SaveChanges();
+            return PartialView();
+        }
+    }
+}
---
+++ MvcCv/Repositories/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using MvcCv.Models.Entity;
+
+namespace MvcCv
+{
+    public class GenericRepository<T> where T : class,new()
+    {
+        DbcvEntities db=new DbcvEntities();
+        public List <T> List()
+        {
+            return db.Set<T>().ToList();
+        }
+        public void TAdd(T p)
+        {
+            db.Set<T>().Add(p);
+            db.SaveChanges();
+        }
+        public void TDelete(T p)
+        {
+            db.Set<T>().Remove(p);
+            db.SaveChanges();
+        }
+        public T TGet(int id)
+        {
+            return db.Set<T>().Find(id);
+        }
+        public void Tupdate(T p)
+        {
+            db.SaveChanges();
+        }
+        public T Find(Expression<Func<T,bool>> where)
+        {
+            return db.Set<T>().FirstOrDefault(where);   
+        }
+    }
+}
